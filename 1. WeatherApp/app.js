2fa9c64df22a5ddcf6117f4342ca5d0aef3fd09d
ar7em1s <slavina.ss94@gmail.com>
2024-07-22 17:21:05 +0300

    init commit

---
+++ 1. WeatherApp/app.js
@@ -0,0 +1,43 @@
+const apiKey = "be7a1082b799fa5d78d50f1892a91c38";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  let data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h";
+
+  switch (data.weather[0].main) {
+    case "Clouds":
+      weatherIcon.src = "img/clouds.png";
+      break;
+    case "Clear":
+      weatherIcon.src = "img/clear.png";
+      break;
+    case "Rain":
+      weatherIcon.src = "img/rain.png";
+      break;
+    case "Drizzle":
+      weatherIcon.src = "img/drizzle.png";
+      break;
+    case "Mist":
+      weatherIcon.src = "img/mist.png";
+      break;
+    case "Snow":
+      weatherIcon.src = "img/snow.png";
+      break;
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
