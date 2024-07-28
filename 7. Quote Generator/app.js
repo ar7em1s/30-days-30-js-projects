68f50166a2cb22805da816bc780b202de8cbbc79
ar7em1s <slavina.ss94@gmail.com>
2024-07-28 14:13:09 +0300

    updt

---
+++ 7. Quote Generator/app.js
@@ -0,0 +1,14 @@
+const apiURL = "https://api.quotable.io/random";
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+async function getQuote(url) {
+  const response = await fetch(url);
+  let data = await response.json();
+
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
+}
+
+getQuote(apiURL);
