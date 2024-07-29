732587eb2cbdf5b4425dc943a5e5bbf2d9692d74
ar7em1s <slavina.ss94@gmail.com>
2024-07-29 12:48:52 +0300

    updt

---
+++ 8. QR Code Generator/app.js
@@ -0,0 +1,9 @@
+const apiUrl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generateQR() {
+  qrImage.src = apiUrl + qrText.value;
+}
