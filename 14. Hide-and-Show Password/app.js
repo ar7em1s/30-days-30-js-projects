98a768735b53b2f77a596659fa703e67b876cb33
ar7em1s <slavina.ss94@gmail.com>
2024-08-05 17:24:46 +0300

    init upld

---
+++ 14. Hide-and-Show Password/app.js
@@ -0,0 +1,12 @@
+let eyeIcon = document.getElementById("eyeicon");
+let password = document.getElementById("password");
+
+eyeIcon.onclick = function () {
+  if (password.type == "password") {
+    password.type = "text";
+    eyeIcon.src = "images/eye-open.png";
+  } else {
+    password.type = "password";
+    eyeIcon.src = "images/eye-close.png";
+  }
+};
