642b3cc604e82eb61e4a5ebe87b6730e4a57f5d0
ar7em1s <slavina.ss94@gmail.com>
2024-08-27 15:06:44 +0300

    capped at 100%

--- 23. Circular Progress Bar/app.js
+++ 23. Circular Progress Bar/app.js
@@ -2,10 +2,10 @@ let number = document.getElementById("number");
 let counter = 0;
 
 setInterval(() => {
-  if (counter == 65) {
+  if (counter == 100) {
     clearInterval();
   } else {
     counter += 1;
     number.innerHTML = counter + "%";
   }
-}, 30);
+}, 19);
